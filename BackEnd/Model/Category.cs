0aad7a3b59eb5d79d6214f235616a880cbf89ef3
Андрей Левченков <lev_a_a@mail.ru>
2025-03-15 09:02:16 +0200

    Update Class Category and Location

--- BackEnd/Model/Category.cs
+++ BackEnd/Model/Category.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace BackEnd.Model;
 
 public partial class Category
 {
+    [JsonPropertyName("categoryid")]
     public int Categoryid { get; set; }
-
+    [JsonPropertyName("name")]
     public string Name { get; set; } = null!;
-
+    [JsonPropertyName("description")]
     public string? Description { get; set; }
-
+    [JsonPropertyName("createdat")]
     public DateTime? Createdat { get; set; }
-
+    [JsonIgnore]
     public virtual ICollection<Device> Devices { get; set; } = new List<Device>();
 }
